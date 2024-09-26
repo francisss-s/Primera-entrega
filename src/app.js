1867e27a5e2ff24d8c440dad9838dc7ca57f88f8
francisco.fuentes <FRANCISCO.FUENTES@WEST-INGENIERIA.CL>
2024-09-26 20:21:19 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8080; 
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+app.use(express.json()); 
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
